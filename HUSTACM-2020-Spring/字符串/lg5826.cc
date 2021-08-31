8934313e81d9e7dd6adf4a8aa04384be8740c6fd
nanami <ymd45921@163.com>
2021-08-31 15:52:15 +0800

    遗留上传

---
+++ HUSTACM-2020-Spring/字符串/lg5826.cc
@@ -0,0 +1,134 @@
+/**
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(string &s) {printf(s.c_str());}
+void print(const char *s) {printf(s);}
+void print(char *s) {printf(s);}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+const int N = 1e5 + 5;
+int a[N], b[N];
+
+void useVector()
+{
+    int type, n, q, m;
+    scanner(type, n, q, m);
+    vector<vector<int>> seq(m + 1);
+    for (int i = 0; i < n; ++ i)
+        scanner(a[i]), seq[a[i]].push_back(i);
+    while (q --)
+    {
+        int l = scanner.nextInt();
+        for (int i = 0; i < l; ++ i)
+            b[i] = scanner.nextInt();
+        int match = 0, now = 0;
+        for (; match < l; ++ match)
+        {
+            auto ii = lower_bound(
+                    seq[b[match]].begin(),
+                    seq[b[match]].end(),
+                    now);
+            if (ii == seq[b[match]].end()) break;
+            else now = *ii + 1;
+        }
+        puts(match == l ? "Yes" : "No");
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    useVector();
+    return 0;
+}
